e4286ee4b31d6abc76fcbf8d5e0936d3a6178b04
张松 <song.z@192.168.0.100>
2021-12-26 21:56:44 +0800

    feat：使用mysql以及jdbcTemplate

---
+++ spring/jdbc/src/main/resources/sql/mysql_schema.sql
@@ -0,0 +1,7 @@
+create table area
+(
+    id   int unsigned NOT NULL AUTO_INCREMENT,
+    code varchar(16) NOT NULL COMMENT '编码',
+    name varchar(16) NOT NULL COMMENT '名称',
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='省市区区域表';
